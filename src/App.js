9ba13b4263c7c387be1797db10dfea72569fc13f
aishaalketbi <aa8680@nyu.edu>
2024-11-22 19:11:36 +0400

    changed home route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 {authToken && <Route path="/dashboard" element={<Dashboard />} />}
                 {authToken && <Route path="/onboarding" element={<OnBoarding />} />}
             </Routes>
